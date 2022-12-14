98e2544ac2f90a5c27836731052b480307dd574b
IliaShkuratov <120378728+IlyaSDeveloper@users.noreply.github.com>
2022-12-14 18:07:49 +0300

    Added products page

---
+++ pages/profile/provider/products/products.js
@@ -0,0 +1,22 @@
+const currentLocation = location.href
+const userLinks = document.querySelectorAll('.navigate-link')
+const productList = document.querySelector('.content-list')
+
+function setActive(linkArray) {
+    linkArray.forEach(link => {
+        if (currentLocation.toLowerCase().includes(link.href.toLowerCase().split('/').slice(-2)[0])) {
+            link.style.fontWeight = 'bold'
+            console.log(link);
+        }
+        else return false
+    });
+}
+setActive(userLinks)
+
+
+// for (let i = 0; index < 6; i++) {
+//     return `<div class='product-item'>
+//         <div class='product-image'></div>
+//     </div>`
+    
+// }
