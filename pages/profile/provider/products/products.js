986179b4b69c087f4327bf0c1fa3bf8247cbcd1d
IliaShkuratov <120378728+IlyaSDeveloper@users.noreply.github.com>
2022-12-15 16:00:51 +0300

    Added sales page

--- pages/profile/provider/products/products.js
+++ pages/profile/provider/products/products.js
@@ -1,21 +1,14 @@
-const currentLocation = location.href
-const userLinks = document.querySelectorAll('.navigate-link')
-const productList = document.querySelector('.content-list')
+// show active link
+// const currentLocation = location.href
+// const userLinks = document.querySelectorAll('.navigate-link')
+// const productList = document.querySelector('.content-list')
 
-function setActive(linkArray) {
-    linkArray.forEach(link => {
-        if (currentLocation.toLowerCase().includes(link.href.toLowerCase().split('/').slice(-2)[0])) {
-            link.style.fontWeight = 'bold'
-        }
-        else return false
-    });
-}
-setActive(userLinks)
-
-
-// for (let i = 0; index < 6; i++) {
-//     return `<div class='product-item'>
-//         <div class='product-image'></div>
-//     </div>`
-    
+// function setActive(linkArray) {
+//     linkArray.forEach(link => {
+//         if (currentLocation.toLowerCase().includes(link.href.toLowerCase().split('/').slice(-2)[0])) {
+//             link.style.fontWeight = 'bold'
+//         }
+//         else return false
+//     });
 // }
+// setActive(userLinks)
