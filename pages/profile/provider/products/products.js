ca63814c59ef5d70ab106f10cb233ca7b82a371c
IliaShkuratov <120378728+IlyaSDeveloper@users.noreply.github.com>
2022-12-15 13:55:06 +0300

    Chanfed structure

--- pages/profile/provider/products/products.js
+++ pages/profile/provider/products/products.js
@@ -6,7 +6,6 @@ function setActive(linkArray) {
     linkArray.forEach(link => {
         if (currentLocation.toLowerCase().includes(link.href.toLowerCase().split('/').slice(-2)[0])) {
             link.style.fontWeight = 'bold'
-            console.log(link);
         }
         else return false
     });
