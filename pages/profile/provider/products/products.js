931d57058c9704d3668f2629c171f45e5185d9e8
IliaShkuratov <120378728+IlyaSDeveloper@users.noreply.github.com>
2022-12-16 15:31:54 +0300

    Cahnged styles for product-create page

--- pages/profile/provider/products/products.js
+++ pages/profile/provider/products/products.js
@@ -1,17 +1,17 @@
-const currentLocation = location.href
-const userLinks = document.querySelectorAll('.navigate-link')
-const productList = document.querySelector('.content-list')
+// const currentLocation = location.href
+// const userLinks = document.querySelectorAll('.navigate-link')
+// const productList = document.querySelector('.content-list')
 
-function setActive(linkArray) {
-    linkArray.forEach(link => {
-        if (currentLocation.toLowerCase().includes(link.href.toLowerCase().split('/').slice(-2)[0])) {
-            link.style.fontWeight = 'bold'
-            console.log(link);
-        }
-        else return false
-    });
-}
-setActive(userLinks)
+// function setActive(linkArray) {
+//     linkArray.forEach(link => {
+//         if (currentLocation.toLowerCase().includes(link.href.toLowerCase().split('/').slice(-2)[0])) {
+//             link.style.fontWeight = 'bold'
+//             console.log(link);
+//         }
+//         else return false
+//     });
+// }
+// setActive(userLinks)
 
 
 // for (let i = 0; index < 6; i++) {
